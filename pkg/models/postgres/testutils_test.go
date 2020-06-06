decd769675510c49f72fc5c9a5e8ede1576b835e
Tommaso Amici <tommaso.amici@me.com>
2020-06-06 14:03:21 +0200

    Moved SQL init in go test set up

--- pkg/models/postgres/testutils_test.go
+++ pkg/models/postgres/testutils_test.go
@@ -1,6 +1,7 @@
 package postgres
 
 import (
+	"context"
 	"testing"
 
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -14,7 +15,16 @@ func newTestDB(t *testing.T) (*pgxpool.Pool, func()) {
 
 	// in a real application you'll want to pass the db url as a flag or environment variable
 	dbURL := "postgresql://test_golang:test_golang@localhost:5432/test_golang"
+
 	pool := NewDB(&dbURL)
+
+	stmtDropTable := "DROP TABLE users;"
+	stmtCreateTable := "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR(128));"
+	stmtInsert := "INSERT INTO users (username) VALUES ('User name');"
+	_, _ := pool.Exec(context.Background(), stmtDropTable)
+	_, _ := pool.Exec(context.Background(), stmtCreateTable)
+	_, _ := pool.Exec(context.Background(), stmtInsert)
+
 	return pool, func() {
 		pool.Close()
 	}
