918469429d6b3aacf80c7bfd5e56b1981d4b2861
Tommaso Amici <tommaso.amici@me.com>
2020-06-06 12:28:01 +0200

    Added error handling when opening db connection

--- pkg/models/postgres/main.go
+++ pkg/models/postgres/main.go
@@ -2,6 +2,8 @@ package postgres
 
 import (
 	"context"
+	"log"
+	"os"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -10,7 +12,9 @@ import (
 func NewDB(dbURL *string) (pool *pgxpool.Pool) {
 	config, _ := pgxpool.ParseConfig(*dbURL)
 
-	// in a real application you'll want to handle errors
-	pool, _ = pgxpool.ConnectConfig(context.Background(), config)
+	pool, err := pgxpool.ConnectConfig(context.Background(), config)
+	if err != nil {
+		log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile).Fatal(err)
+	}
 	return
 }
